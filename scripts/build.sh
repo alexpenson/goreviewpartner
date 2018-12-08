0cf11e85cb572540c4570da7d89876592a0c239d
Alex Penson <pensona@mskcc.org>
2018-12-08 14:45:22 -0500

    don't use gnu-sed

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,11 +3,11 @@ pip install -U py2app dmgbuild wxPython
 python setup.py py2app
 source scripts/make-leelaz.sh
 curl -O https://zero.sjeng.org/best-network
-gsed -i \
-  -e '90s|^.*$|command = ./leelaz|'\
-  -e '91s|^.*$|parameters = --gtp --noponder --weights best-network|'\
-  -e '96s|^.*$|command = ./leelaz|'\
-  -e '97s|^.*$|parameters = --gtp --noponder --weights best-network|' \
-  config.ini
+sed \
+    -e '90s|^.*$|command = ./leelaz|' \
+    -e '91s|^.*$|parameters = --gtp --noponder --weights best-network|' \
+    -e '96s|^.*$|command = ./leelaz|' \
+    -e '97s|^.*$|parameters = --gtp --noponder --weights best-network|' \
+    config.ini > config.ini_ && mv config.ini_ config.ini
 python2.7 leela_zero_analysis.py --no-gui tests/example.sgf
 mv config.ini best-network leelaz dist/GoReviewPartner.app/Contents/MacOS/
--- scripts/deps.sh
+++ scripts/deps.sh
@@ -2,6 +2,5 @@
 # brew update
 # brew list python &>/dev/null || brew install python
 pip install virtualenv
-brew install gnu-sed
 virtualenv --python=python2.7 --distribute --no-site-packages --unzip-setuptools env
 . ./env/bin/activate
